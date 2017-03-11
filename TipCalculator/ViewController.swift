6f51c15482948158487af91348888f10e331d282
Zhuodi Liu <zhuodiliu@gmail.com>
2017-03-11 10:59:28 -0800

    Add fundamental functions

--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tipValue: UILabel!
+    @IBOutlet weak var totalValue: UILabel!
+    @IBOutlet weak var percentSelect: UISegmentedControl!
+    @IBOutlet weak var inputValue: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func tapOnScreen(sender: AnyObject) {
+        view.endEditing(true)
+    }
 
+    @IBAction func calculateTip(sender: AnyObject) {
+        let value = Double(inputValue.text!) ?? 0.0
+        let tipPercent = [0.15, 0.18, 0.20]
+        let tip = value * tipPercent[percentSelect.selectedSegmentIndex]
+        let sum = value + tip
+        tipValue.text = String(format: "$%.2f", tip)
+        totalValue.text = String(format: "$%.2f", sum)
+    }
 }
 
