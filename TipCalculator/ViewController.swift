6d0dd196b450205dff6d0bea47f9f352c03590c7
Zhuodi Liu <zhuodiliu@gmail.com>
2017-03-11 11:57:20 -0800

    Add average

--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -14,6 +14,11 @@ class ViewController: UIViewController {
     @IBOutlet weak var totalValue: UILabel!
     @IBOutlet weak var percentSelect: UISegmentedControl!
     @IBOutlet weak var inputValue: UITextField!
+    @IBOutlet weak var companions: UILabel!
+    @IBOutlet weak var addCompanionOp: UIStepper!
+    @IBOutlet weak var averageBill: UILabel!
+    var numberOfCompanions = 1.0
+    var sum = 0.0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,13 +34,25 @@ class ViewController: UIViewController {
         view.endEditing(true)
     }
 
+    @IBAction func addCompanions(sender: UIStepper) {
+        let add = addCompanionOp.value
+        numberOfCompanions = add
+        let average = sum / numberOfCompanions
+        companions.text = String(Int(numberOfCompanions))
+        averageBill.text = String(format: "$%.2f", average)
+    }
+    
+    
     @IBAction func calculateTip(sender: AnyObject) {
         let value = Double(inputValue.text!) ?? 0.0
         let tipPercent = [0.15, 0.18, 0.20]
         let tip = value * tipPercent[percentSelect.selectedSegmentIndex]
-        let sum = value + tip
+        sum = value + tip
         tipValue.text = String(format: "$%.2f", tip)
         totalValue.text = String(format: "$%.2f", sum)
+        let average = sum / numberOfCompanions
+        companions.text = String(Int(numberOfCompanions))
+        averageBill.text = String(format: "$%.2f", average)
     }
 }
 
